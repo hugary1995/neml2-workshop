07fda2b98df308cdc392ba5f37e753ccacbd917a
Gary Hu <thu@anl.gov>
2023-12-11 08:54:07 -0600

    Switch back to pdf as our printer does not recognize ps...

--- print_examples.py
+++ print_examples.py
@@ -5,7 +5,7 @@ import shutil
 import subprocess
 
 ex_dir = Path("examples")
-out_dir = Path("examples-ps")
+out_dir = Path("examples-pdf")
 language = {".h": "cpp", ".cxx": "cpp", ".i": "python"}
 
 
@@ -13,6 +13,10 @@ if __name__ == "__main__":
     if not shutil.which("enscript"):
         raise Exception(
             "'enscript' will be used to convert source code to PostScript, but it cannot be found in PATH."
+        )
+    if not shutil.which("ps2pdf"):
+        raise Exception(
+            "'ps2pdf' will be used to convert PostScript to PDF, but it cannot be found in PATH."
         )
 
     if out_dir.exists():
@@ -40,3 +44,16 @@ if __name__ == "__main__":
         subprocess.run(
             args, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
         )
+
+        # PS to pdf
+        args = [
+            "ps2pdf",
+            str(ofile) + ".ps",
+            str(ofile) + ".pdf",
+        ]
+        print(" ".join(args))
+        subprocess.run(
+            args, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+        )
+
+        print("-" * 79)
